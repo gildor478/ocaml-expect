a9c0aa3218e68d1c4ec67ead7e76a8122aecc0fe
sylvain.le-gall <sylvain.le-gall@polytechnique.org>
2013-10-26 01:13:40 +0200

    Port test to OUnit 2.0.0.
    Ignore-this: cd00e27a577c6187a8397cf56e1411cc
    
    darcs-hash:20131025231340-4d692-7ebd98edf8518045dc91e8cebc25087418d36a95

--- test/test.ml
+++ test/test.ml
@@ -20,149 +20,141 @@
 (* Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA              *)
 (******************************************************************************)
 
-
 (** Run tests for Expect
     @author Sylvain Le Gall
   *)
 
-open OUnit
+open OUnit2
 open Expect
 open TestCommon
 
-let () =
-  let timeout =
-    0.1
+let timeout = 0.1
+
+let with_qa test_ctxt ?use_stderr ?(exit_code=Unix.WEXITED 0) suite f =
+  let _, real_exit_code =
+    with_spawn
+      ~verbose:true
+      ~verbose_output:(logf test_ctxt `Info "%s")
+      ?use_stderr
+      ~timeout:(Some timeout) "_build/test/qa.byte" [|suite|]
+      (fun t () -> f t) ()
   in
-  let with_qa ?use_stderr ?(exit_code=Unix.WEXITED 0) suite f =
-    let _, real_exit_code =
-      with_spawn
-(*         ~verbose:true  *)
-        ?use_stderr
-        ~timeout:(Some timeout) "_build/test/qa.byte" [|suite|]
-        (fun t () -> f t) ()
-    in
-      assert_equal
-        ~msg:"Exit code"
-        ~printer:printer_exit_code
-        exit_code
-        real_exit_code
-  in
+    assert_equal
+      ~msg:"Exit code"
+      ~printer:printer_exit_code
+      exit_code
+      real_exit_code
 
-  let tests =
-    "Expect" >:::
-    [
-      "std-str" >::
-      (fun () ->
-         with_qa "std"
-           (fun t ->
-              assert_bool
-                "Ask name"
-                (ExpectStr.expect t
-                   [`Regexp
-                      (Str.regexp (Str.quote "What's your name? ")),
-                    true]
-                   false);
+let tests =
+  "Expect" >:::
+  [
+    "std-str" >::
+    (fun test_ctxt ->
+       with_qa test_ctxt "std"
+         (fun t ->
+            assert_bool
+              "Ask name"
+              (ExpectStr.expect t
+                 [`Regexp
+                    (Str.regexp (Str.quote "What's your name? ")),
+                  true]
+                 false);
 
-              send t "Sylvain\n";
+            send t "Sylvain\n";
 
-              assert_bool
-                "Answer hello"
-                (ExpectStr.expect t
-                   [`Regexp
-                      (Str.regexp (Str.quote "Hello Sylvain")),
-                    true]
-                   false)));
+            assert_bool
+              "Answer hello"
+              (ExpectStr.expect t
+                 [`Regexp
+                    (Str.regexp (Str.quote "Hello Sylvain")),
+                  true]
+                 false)));
 
-      "std-pcre" >::
-      (fun () ->
-         with_qa "std"
-           (fun t ->
-              assert_bool
-                "Ask name"
-                (ExpectPcre.expect t
-                   [`Pat "What's your name\\? ", true]
-                   false);
+    "std-pcre" >::
+    (fun test_ctxt ->
+       with_qa test_ctxt "std"
+         (fun t ->
+            assert_bool
+              "Ask name"
+              (ExpectPcre.expect t
+                 [`Pat "What's your name\\? ", true]
+                 false);
 
-              send t "Sylvain\n";
+            send t "Sylvain\n";
 
-              assert_bool
-                "Answer hello"
-                (ExpectPcre.expect t
-                   [`Pat "Hello Sylvain", true]
-                   false)));
+            assert_bool
+              "Answer hello"
+              (ExpectPcre.expect t
+                 [`Pat "Hello Sylvain", true]
+                 false)));
 
-      "oasis1" >::
-      (fun () ->
-         with_qa "oasis1"
-           (fun t ->
-              assert_bool
-                "???name "
-                (expect t
-                   [`Exact "???name ", true]
-                   false);
-              send t "test\n"));
+    "oasis1" >::
+    (fun test_ctxt ->
+       with_qa test_ctxt "oasis1"
+         (fun t ->
+            assert_bool
+              "???name "
+              (expect t
+                 [`Exact "???name ", true]
+                 false);
+            send t "test\n"));
 
-      "oasis1-withfmatch" >::
-      (fun () ->
-         with_qa "oasis1"
-           (fun t ->
-              assert_bool
-                "???name "
-                (expect t
-                   ~fmatches:[(fun str ->
-                                 if str = "???name " then
-                                   Some true
-                                 else
-                                   None)]
-                   []
-                   false);
-              send t "test\n"));
+    "oasis1-withfmatch" >::
+    (fun test_ctxt ->
+       with_qa test_ctxt "oasis1"
+         (fun t ->
+            assert_bool
+              "???name "
+              (expect t
+                 ~fmatches:[(fun str ->
+                               if str = "???name " then
+                                 Some true
+                               else
+                                 None)]
+                 []
+                 false);
+            send t "test\n"));
 
-      "oasis1-timeout" >::
-      (fun () ->
-         with_qa ~exit_code:(Unix.WEXITED 2) "oasis1"
-           (fun t ->
-              let mtx =
-                Mutex.create ()
-              in
-              let finished =
-                ref false
-              in
-              let th =
-                Thread.create
-                  (fun () ->
-                     let _b : bool =
-                       expect t [`Exact "toto", true] false
-                     in
-                       Mutex.lock mtx;
-                       finished := true;
-                       Mutex.unlock mtx)
-                  ()
-              in
-              let is_finished =
-                Thread.delay (5. *. timeout);
-                Mutex.lock mtx;
-                !finished
-              in
-                Mutex.unlock mtx;
-                if not is_finished then
-                  assert_failure "Process didn't timeout";
-                Thread.join th));
-      "stderr" >::
-      (fun () ->
-         with_qa ~use_stderr:true "stderr"
-           (fun t ->
-              assert_bool
-                "error"
-                (expect t
-                   [`Exact "error", true]
-                   false)));
-    ]
-  in
+    "oasis1-timeout" >::
+    (fun test_ctxt ->
+       with_qa test_ctxt ~exit_code:(Unix.WEXITED 2) "oasis1"
+         (fun t ->
+            let mtx =
+              Mutex.create ()
+            in
+            let finished =
+              ref false
+            in
+            let th =
+              Thread.create
+                (fun () ->
+                   let _b : bool =
+                     expect t [`Exact "toto", true] false
+                   in
+                     Mutex.lock mtx;
+                     finished := true;
+                     Mutex.unlock mtx)
+                ()
+            in
+            let is_finished =
+              Thread.delay (5. *. timeout);
+              Mutex.lock mtx;
+              !finished
+            in
+              Mutex.unlock mtx;
+              if not is_finished then
+                assert_failure "Process didn't timeout";
+              Thread.join th));
 
-  let _lst =
-    run_test_tt_main tests
-  in
+    "stderr" >::
+    (fun test_ctxt ->
+       with_qa test_ctxt ~use_stderr:true "stderr"
+         (fun t ->
+            assert_bool
+              "error"
+              (expect t
+                 [`Exact "error", true]
+                 false)));
+  ]
 
-    ()
-
+let () = run_test_tt_main tests
